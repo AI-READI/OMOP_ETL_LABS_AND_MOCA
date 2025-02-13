e9ff16984398bb982fd66495282d8262f61b7b77
chambest <chambest@ohsu.edu>
2025-02-12 17:47:46 -0800

    Add files via upload

--- moca_etl_parameters.py
+++ moca_etl_parameters.py
@@ -1,5 +1,7 @@
 # postgres database connection parameters
-POSTGRES_CONN_STRING_KEY='postgresql://cohenaa@localhost:5432/omop'
+#POSTGRES_CONN_STRING_KEY='postgresql://cohenaa@localhost:5432/omop'
+#POSTGRES_CONN_STRING_KEY='postgresql://postgres:$postgres@10.0.0.4/omop?options=-csearch_path%3Daireadi_omop'
+POSTGRES_CONN_STRING_KEY='postgresql://postgres:$postgres@10.1.0.6/omop?options=-csearch_path%3Daireadi_omop'
 
 # postgres OMOP read and write table names within correct schema
 POSTGRES_OMOP_READ_PERSON_TABLE_NAME='aireadi_omop.person'
@@ -10,13 +12,24 @@ POSTGRES_MOCA_WRITE_MEASUREMENT_TABLE_NAME='measurement'
 POSTGRES_MOCA_WRITE_OBSERVATION_TABLE_NAME='observation'
 
 # path to the standard mapping csv file
-STANDARDS_MAPPING_CSV_PATH='./MOCA/STANDARDS_MAPPINGS/0112_2024_06_20_MOCA_Data_Dictionary_Mappings.csv'
+STANDARDS_MAPPING_CSV_PATH='/home/azureuser/omop_etl_labs_and_moca/MOCA/STANDARDS_MAPPINGS/MOCA_Data_Dictionary_Mappings_9.30.24.csv'
 
 # glob wildcard path to the raw MOCA source data files
-MOCA_SOURCE_DATA_GLOB = './MOCA/EXAMPLE_DATA/MOCA-latest.csv;./MOCA/EXAMPLE_DATA/MOCA-latest-Paper.csv'
+MOCA_SOURCE_DATA_GLOB = '/home/azureuser/data/moca/MOCA-latest.csv;/home/azureuser/data/moca/MOCA-latest-Paper.csv'
 
 # control writing to the OMOP database, for debugging
-MOCA_OMOP_WRITE_TO_DATABASE = True
+MOCA_OMOP_WRITE_TO_DATABASE = True 
+
+##SRC:  This points to the Redcap extract file
+redcap_report='/home/azureuser/data/redcap/Redcap_data_report_270041.csv'
+
+
+
+
+
+
+
+
 
 
 
