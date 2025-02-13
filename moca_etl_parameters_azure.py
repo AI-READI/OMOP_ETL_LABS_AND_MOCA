6384195a2b1b9b40bc85958d8f2623fc343467cc
chambest <chambest@ohsu.edu>
2025-02-12 17:47:21 -0800

    Add files via upload

--- moca_etl_parameters_azure.py
+++ moca_etl_parameters_azure.py
@@ -12,13 +12,16 @@ POSTGRES_MOCA_WRITE_MEASUREMENT_TABLE_NAME='measurement'
 POSTGRES_MOCA_WRITE_OBSERVATION_TABLE_NAME='observation'
 
 # path to the standard mapping csv file
-STANDARDS_MAPPING_CSV_PATH='/home/azureuser/omop_etl_labs_and_moca/MOCA/STANDARDS_MAPPINGS/0112_2024_06_20_MOCA_Data_Dictionary_Mappings.csv'
+STANDARDS_MAPPING_CSV_PATH='/home/azureuser/omop_etl_labs_and_moca/MOCA/STANDARDS_MAPPINGS/MOCA_Data_Dictionary_Mappings_9.30.24.csv'
 
 # glob wildcard path to the raw MOCA source data files
 MOCA_SOURCE_DATA_GLOB = '/home/azureuser/data/moca/MOCA-latest.csv;/home/azureuser/data/moca/MOCA-latest-Paper.csv'
 
 # control writing to the OMOP database, for debugging
 MOCA_OMOP_WRITE_TO_DATABASE = True 
+
+##SRC:  This points to the Redcap extract file
+redcap_report='/home/azureuser/data/redcap/Redcap_data_report_270041.csv'
 
 
 
